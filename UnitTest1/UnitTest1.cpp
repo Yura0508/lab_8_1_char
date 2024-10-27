3bf2607253c7f662a5fcd91e0144e0e0255aecfd
Yura <urijkisil2007@gmail.com>
2024-10-27 20:20:07 +0200

    first master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8_1_char/lab_8_1_char.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestCount)
+		{
+			char str1[] = "while we wait, while working.";
+			char str2[] = "no while here";
+			char str3[] = "whilewhile";
+			char str4[] = "nothing";
+
+			Assert::AreEqual(2, Count(str1, "while"));
+			Assert::AreEqual(1, Count(str2, "while"));
+			Assert::AreEqual(2, Count(str3, "while"));
+			Assert::AreEqual(0, Count(str4, "while"));
+		}
+		TEST_METHOD(TestChange)
+		{
+			char str1[] = "while we wait, while working.";
+			char expected1[] = "*** we wait, *** working.";
+			char str2[] = "no while here";
+			char expected2[] = "no *** here";
+			char str3[] = "whilewhile";
+			char expected3[] = "******";
+
+			// Виконуємо заміну
+			Change(str1);
+			Change(str2);
+			Change(str3);
+
+			// Перевіряємо результати
+			Assert::AreEqual(expected1, str1);
+			Assert::AreEqual(expected2, str2);
+			Assert::AreEqual(expected3, str3);
+		}
+
+	};
+}
---
+++ lab_8_1_char/lab_8_1_char.cpp
@@ -0,0 +1,59 @@
+﻿// lab_8_1_char.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* s, const char* cs)
+{
+	int k = 0,
+		pos = 0;
+	char* t;
+	while (t = strstr(s + pos, cs))
+	{
+		pos = t - s + 1;
+		k++;
+	}
+	return k;
+}
+char* Change(char* s)
+{
+	char* t = new char[strlen(s)];
+	char* p;
+	int pos1 = 0,
+		pos2 = 0;
+	*t = 0;
+	while (p = strchr(s + pos1, 'w'))
+	{
+		if (s[p - s + 1] == 'h' && s[p - s + 2] == 'i' && s[p - s + 3] == 'l' && s[p - s + 4] == 'e')
+		{
+			pos2 = p - s + 5;
+			strncat(t, s + pos1, pos2 - pos1 - 5);
+			strcat(t, "***");
+			pos1 = pos2;
+		}
+		else
+		{
+			pos2 = p - s + 3;
+			strncat(t, s + pos1, pos2 - pos1);
+			pos1 = pos2;
+		}
+	}
+	strcat(t, s + pos1);
+	strcpy(s, t);
+	return t;
+}
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	char cs[] = "while";
+	cout << "String contained " << Count(str, cs) << " groups of 'while'" << endl;
+	char* dest = new char[101];
+	dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
